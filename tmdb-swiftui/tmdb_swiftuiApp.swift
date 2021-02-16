b8881d52c4b2246921a3f71de0982ecc90643b61
MCOMM00013 <muhammad.rahmatul@mncgroup.com>
2021-02-16 13:36:43 +0700

    init

---
+++ tmdb-swiftui/tmdb_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tmdb_swiftuiApp.swift
+//  tmdb-swiftui
+//
+//  Created by Muhammad Khair on 16/02/21.
+//
+
+import SwiftUI
+
+@main
+struct tmdb_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
